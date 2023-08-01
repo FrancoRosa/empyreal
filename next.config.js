140f8e1e7743127a4b39b2b96f40309c1213688e
FrancoRosa <km115.franco@gmail.com>
2023-08-01 16:08:22 -0500

    Update to production keys

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    VISA_COM: "456879853",
-    VISA_USR: "integraciones@niubiz.com.pe",
-    VISA_PSS: "_7z3@8fF",
-    VISA_ENV: "testing",
+    VISA_COM: "650237285",
+    VISA_USR: "support@rtklink.com",
+    VISA_PSS: "UpI3pn?n",
+    VISA_ENV: "production",
     SUP_URL: "https://bucwgvzgaolztuunhlly.supabase.co",
     SUP_ANN:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1Y3dndnpnYW9senR1dW5obGx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkxODA0ODMsImV4cCI6MjAwNDc1NjQ4M30.6dL0sS5UM-9Y5eKd3-zAQElpJ-ALoLKFoavLXukUIXY",
