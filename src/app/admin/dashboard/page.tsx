82c931d5f8a3cb2f51bad169098f8ac386c8a12c
FrancoRosa <km115.franco@gmail.com>
2023-08-04 09:04:05 -0500

    Edit product form

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -44,68 +44,66 @@ export default function Admin() {
           <h1 className="text-center mb-4 text-gray-700 uppercase font-bold text-xl  dark:bg-gray-700 dark:text-gray-400">
             Products
           </h1>
-          <div className="relative overflow-x-auto">
-            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                  <th scope="col" className="px-2 py-1">
-                    Item
-                  </th>
-                  <th scope="col" className="px-2 py-1">
-                    Product name
-                  </th>
-                  <th scope="col" className="px-2 py-1">
-                    Description
-                  </th>
-                  <th scope="col" className="px-2 py-1">
-                    Images
-                  </th>
-                  <th scope="col" className="px-2 py-1">
-                    Stock
-                  </th>
-                  <th scope="col" className="px-2 py-1">
-                    Price
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {products.map((product: any, index: number) => (
-                  <>
-                    <tr
-                      key={index}
-                      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 cursor-pointer text-xs"
-                      onClick={() => {
-                        handleEdit(product);
-                      }}
+          <table className="mx-12 text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-2 py-1">
+                  Item
+                </th>
+                <th scope="col" className="px-2 py-1">
+                  Product name
+                </th>
+                <th scope="col" className="px-2 py-1">
+                  Description
+                </th>
+                <th scope="col" className="px-2 py-1">
+                  Images
+                </th>
+                <th scope="col" className="px-2 py-1">
+                  Stock
+                </th>
+                <th scope="col" className="px-2 py-1">
+                  Price
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product: any, index: number) => (
+                <>
+                  <tr
+                    key={index}
+                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 cursor-pointer text-xs"
+                    onClick={() => {
+                      handleEdit(product);
+                    }}
+                  >
+                    <td className="px-2 py-1">{index + 1}</td>
+                    <th
+                      scope="row"
+                      className="px-2 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                     >
-                      <td className="px-2 py-1">{index + 1}</td>
-                      <th
-                        scope="row"
-                        className="px-2 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                      >
-                        {product.name}
-                      </th>
-                      <td className="px-2 py-1">{product.description}</td>
-                      <td className="px-2 py-1">{product.images.length}</td>
-                      <td className="px-2 py-1">{product.stock}</td>
-                      <td className="px-2 py-1 text text-right">
-                        {product.price}
-                      </td>
-                    </tr>
-                    <tr
-                      className={
-                        !edit || product.id !== target.id ? "hidden" : ""
-                      }
-                    >
-                      <td colSpan={6}>
-                        <EditProduct product={product} />
-                      </td>
-                    </tr>
-                  </>
-                ))}
-              </tbody>
-            </table>
-          </div>
+                      {product.name}
+                    </th>
+                    <td className="px-2 py-1">{product.description}</td>
+                    <td className="px-2 py-1">{product.images.length}</td>
+                    <td className="px-2 py-1">{product.stock}</td>
+                    <td className="px-2 py-1 text text-right">
+                      {product.price}
+                    </td>
+                  </tr>
+                  <tr
+                    className={
+                      !edit || product.id !== target.id ? "hidden" : ""
+                    }
+                  >
+                    <td colSpan={6}>
+                      <EditProduct product={product} />
+                    </td>
+                  </tr>
+                </>
+              ))}
+            </tbody>
+          </table>
         </div>
       )}
     </section>
--- src/components/edit_product.tsx
+++ src/components/edit_product.tsx
@@ -9,13 +9,67 @@ export const EditProduct = ({ product }: { product: any }) => {
   };
   return (
     <>
-      <form className="border border-red-400">
-        <input
-          type="text"
-          id="name"
-          value={form.name}
-          onChange={handleChange}
-        />
+      <form className="border border-red-400 px-8 flex-col flex text-sm">
+        <div className="inline-flex">
+          <p className="w-24 font-semibold">Name: </p>
+          <input
+            className="w-96"
+            type="text"
+            id="name"
+            value={form.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="inline-flex">
+          <label className="w-24 font-semibold align-top">Description: </label>
+          <textarea
+            rows={5}
+            cols={50}
+            id="description"
+            value={form.description}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="inline-flex">
+          <label className="w-24 font-semibold">Price: </label>
+          <input
+            className="w-96"
+            type="number"
+            id="price"
+            value={form.price}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="inline-flex">
+          <label className="w-24 font-semibold">Stock: </label>
+          <input
+            className="w-96"
+            type="number"
+            id="stock"
+            value={form.stock}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="inline-flex">
+          <label className="w-24 font-semibold">Datasheet: </label>
+          <input
+            className="w-96"
+            type="text"
+            id="datasheet"
+            value={form.datasheet}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="inline-flex">
+          <label className="w-24 font-semibold">Manual: </label>
+          <input
+            className="w-96"
+            type="text"
+            id="manual"
+            value={form.manual}
+            onChange={handleChange}
+          />
+        </div>
       </form>
       <pre className="text-xs font-mono overflow-hidden w-screen">
         {JSON.stringify(product, null, 2)}
